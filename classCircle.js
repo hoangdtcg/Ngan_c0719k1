de64d52acdb3b9b908cca14c90c85ca0a20956ed
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-27 17:32:12 +0700

    final

---
+++ classCircle.js
@@ -0,0 +1,66 @@
+let Ball = function () {
+    this.x = canvas.width / 2;//toa do theo truc X
+    this.radius = 5;//ban kinh
+    this.y = canvas.height - 60 - this.radius;//toa do theo truc Y
+    this.dx = 2;//delta x
+    this.dy = 2;//delta y
+    this.color = 'red';
+    this.live = 3;
+    this.moveX = function () {
+        this.x -= this.dx;
+    };
+
+    this.moveY = function () {
+        this.y -= this.dy;
+    };
+
+    this.moveball = function () {
+        this.moveX();
+        this.moveY();
+        if (this.x < this.radius || this.x > canvas.width - this.radius) {
+            this.dx = -this.dx;
+        }
+        if (this.y < this.radius) {
+            this.dy = -this.dy;
+        }
+    };
+//ve hinh tron
+    this.draw = function (context) {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        context.fillStyle = this.color;
+        context.fill();
+        context.closePath();
+    };
+//so luot choi
+    this.liveBall = function () {
+        if (this.y + this.radius > canvas.height) {
+            this.live--;
+            this.resetBall();
+        }
+    };
+    this.getlive = function () {
+        return this.live;
+    }
+    this.resetBall = function () {
+        this.x = canvas.width / 2;
+        this.y = canvas.height - 60 - this.radius;
+        this.dx = 4;
+        this.dy = 4;
+    };
+
+    this.showLive = function (context) {
+        context.font = "20px Arial";
+        context.fillStyle = "orange";
+        context.fillText("Live: " + this.getlive(), 400, 20)
+    }
+    this.showgameOver = function (context) {
+        if (this.getlive() <= 0) {
+            context.font = "40px Arial";
+            context.fillStyle = "red";
+            context.textAlign = "center";
+            context.fillText("GAME OVER!", canvas.width / 2, canvas.height / 2);
+        }
+    }
+};
+
---
+++ main.js
@@ -0,0 +1,123 @@
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+let isGameover = false;
+let ball = new Ball();
+let taskbar = new Taskbar();
+let bricks = new Bricks();
+let brickarr = [];
+let img = new Image();
+
+
+// luu toa do vien gach trong mang
+for (let i = 0; i < bricks.totalRow; i++) {
+    brickarr[i] = [];
+    for (let j = 0; j < bricks.totalCol; j++) {
+        brickarr[i][j] =
+            {
+                x: bricks.getX(bricks.setX(j)),
+                y: bricks.getY(bricks.setY(i)),
+                isbroken: bricks.getStatusbrick()
+            }
+    }
+}
+//ve cac vien gach len man hinh
+function createBrick() {
+    for (let i = 0; i < bricks.totalRow; i++) {
+        for (let j = 0; j < bricks.totalCol; j++) {
+            if (!brickarr[i][j].isbroken) {
+                bricks.draw(ctx, brickarr[i][j].x, brickarr[i][j].y);
+            }
+        }
+    }
+
+}
+
+//xu ly su kien bong dap vao gach
+function balltouchbricks() {
+    for (let i = 0; i < bricks.totalRow; i++) {
+        for (let j = 0; j < bricks.totalCol; j++) {
+            if (!brickarr[i][j].isbroken) {
+
+                if (ball.x +ball.radius >= brickarr[i][j].x && ball.x +ball.radius <= brickarr[i][j].x + bricks.width &&
+                    ball.y + ball.radius >= brickarr[i][j].y && ball.y + ball.radius <= brickarr[i][j].y + bricks.height) {
+                    ball.dy = -ball.dy;
+                    bricks.getScore(bricks.setScore(1));
+                    brickarr[i][j].isbroken = true;
+                }
+            }
+        }
+    }
+}
+//su kien nha phim
+function onKeyup(event) {
+    switch (event.which) {
+        case 37:
+            taskbar.isleft = false;
+            break;
+        case 39:
+            taskbar.isright = false;
+            break;
+        default:
+            break;
+    }
+}
+//su kien nhan phim
+function onKeydown(event) {
+    switch (event.which) {
+        case 37:
+            taskbar.isleft = true;
+            break;
+        case 39:
+            taskbar.isright = true;
+            break;
+        default:
+            break;
+    }
+}
+
+document.addEventListener('keyup', onKeyup);
+document.addEventListener('keydown', onKeydown);
+//kiem tra game ket thuc hay chua
+function checkGameover() {
+    if (ball.y > canvas.height - ball.radius) {
+        ball.liveBall();
+        taskbar.resetTaskbar();
+        if (ball.getlive() <= 0) {
+            isGameover = true;
+            ball.showgameOver(ctx);
+        }
+
+    }
+}
+//kiem tra chien thang
+function checkWin() {
+    if(bricks.getScore()>=bricks.totalCol*bricks.totalRow)
+    {
+        bricks.showWin(ctx);
+        cancelAnimationFrame(playGame());
+    }
+}
+function loadImage() {
+    img.src = "image/totoro.jpeg";
+    ctx.drawImage(img, 0, 0, 500, 600);
+}
+function playGame() {
+    if (!isGameover) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+       loadImage();
+        ball.draw(ctx);
+        taskbar.draw(ctx);
+        ball.moveball();
+        taskbar.movetaskBar();
+        createBrick();
+        taskbar.touchBall(ball);
+        balltouchbricks();
+        bricks.showScore(ctx);
+        ball.showLive(ctx);
+        checkWin();
+        checkGameover();
+        requestAnimationFrame(playGame);
+    }
+
+}
+
